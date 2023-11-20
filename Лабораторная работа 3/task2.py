43b16d623285dae1aee878558199fc96808298db
o0xx1 <148235211+o0xx1@users.noreply.github.com>
2023-11-20 22:08:46 +0300

    Update task2.py

--- Лабораторная работа 3/task2.py
+++ Лабораторная работа 3/task2.py
@@ -4,11 +4,8 @@ participants_second_group = "Петров|Сидоров|Смирнов"
 def find_common_participants(participants_first_group, participants_second_group, sep=','):
     first_group = participants_first_group.split(sep)
     second_group = participants_second_group.split(sep)
-    common_participants_list = []  # список общих участников среди двух групп
-    for participants in first_group:
-        if participants in second_group:
-            common_participants_list.append(participants)
-            common_participants_list.sort()
+    common_participants_list = list(set(first_group).intersection(second_group))  # список общих участников среди двух групп
+    common_participants_list.sort()
     return common_participants_list
 # TODO Провеьте работу функции с разделителем отличным от запятой
 print(find_common_participants(participants_first_group, participants_second_group, sep='|'))
